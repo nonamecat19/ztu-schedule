86c07ea5940a8ef91ca43e42e1d562c082243c0f
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-04-04 13:07:14 +0300

    updated README.md

--- pages/api/cabinet.ts
+++ pages/api/cabinet.ts
@@ -32,56 +32,56 @@ async function getCabinet(week?, day?): Promise<CabinetInfo> {
     const loginPage = "https://cabinet.ztu.edu.ua/site/login"
 
     const browser = await puppeteer.launch()
-    // const page = await browser.newPage()
-    // await page.goto(loginPage)
-    // // @ts-ignore
-    // await page.$eval("#loginform-username", (el) => el.value = "vt211_zks")
-    // // @ts-ignore
-    // await page.$eval("#loginform-password", (el) => el.value = "700170")
-    // await page.click("#login-form > div:nth-child(6) > button")
-    // await page.waitForSelector("body > div > div > div.site-index > div > div > a:nth-child(2)")
-    // await page.goto(url)
-    //
-    // const exp = await page.$eval("body > div.wrap > div", (el) => el.innerHTML)
-    // let ex = await page.$eval("body > div.wrap > div", (el) => el.innerHTML)
-    // let ind = ex.indexOf("<h2>")
-    // let ind2 = ex.indexOf("<style>")
-    // ex = ex.substring(ind, ind2)
-    // // @ts-ignore
-    // ex = ex.split("<div style=\"color:#DDD; display: none;\">")
-    //
+    const page = await browser.newPage()
+    await page.goto(loginPage)
+    // @ts-ignore
+    await page.$eval("#loginform-username", (el) => el.value = "vt211_zks")
+    // @ts-ignore
+    await page.$eval("#loginform-password", (el) => el.value = "700170")
+    await page.click("#login-form > div:nth-child(6) > button")
+    await page.waitForSelector("body > div > div > div.site-index > div > div > a:nth-child(2)")
+    await page.goto(url)
+
+    const exp = await page.$eval("body > div.wrap > div", (el) => el.innerHTML)
+    let ex = await page.$eval("body > div.wrap > div", (el) => el.innerHTML)
+    let ind = ex.indexOf("<h2>")
+    let ind2 = ex.indexOf("<style>")
+    ex = ex.substring(ind, ind2)
+    // @ts-ignore
+    ex = ex.split("<div style=\"color:#DDD; display: none;\">")
+
     let lessons = []
-    // let para = 1
-    // try {
-    //     const $ = cheerio.load(exp)
-    //     const day = $(".pair")
-    //
-    //     day.each(function () {
-    //
-    //         let time = $(this).find(".pair div:nth-child(1) div:nth-child(1)").text()
-    //         let subject = $(this).find(".pair div:nth-child(1) div:nth-child(2)").text()
-    //         let type = $(this).find(".pair div:nth-child(1) div:nth-child(3)").text()
-    //         let room = $(this).find(".pair div:nth-child(1) div:nth-child(4)").text()
-    //         let teacher = $(this).find(".pair div:nth-child(1) div:nth-child(5)").text()
-    //
-    //         let temp = ex[para].split("</div>")
-    //         let text = temp[0].trim()
-    //         let links = temp[1]
-    //         links = links.replace("<div style=\"font-size:1.5em;\">", "").trim()
-    //         para++
-    //
-    //         lessons.push({time, subject, type, room, teacher, text, links})
-    //     })
-    //
-    // } catch (err) {
-    //     console.error(err)
-    // }
-    // await browser.close()
-    //
-    // if (lessons.length >= 2) {
-    //     if (equalObject(lessons[lessons.length - 1], lessons[lessons.length - 2]))
-    //         return lessons.slice(0, lessons.length - 1)
-    //     return lessons
-    // }
+    let para = 1
+    try {
+        const $ = cheerio.load(exp)
+        const day = $(".pair")
+
+        day.each(function () {
+
+            let time = $(this).find(".pair div:nth-child(1) div:nth-child(1)").text()
+            let subject = $(this).find(".pair div:nth-child(1) div:nth-child(2)").text()
+            let type = $(this).find(".pair div:nth-child(1) div:nth-child(3)").text()
+            let room = $(this).find(".pair div:nth-child(1) div:nth-child(4)").text()
+            let teacher = $(this).find(".pair div:nth-child(1) div:nth-child(5)").text()
+
+            let temp = ex[para].split("</div>")
+            let text = temp[0].trim()
+            let links = temp[1]
+            links = links.replace("<div style=\"font-size:1.5em;\">", "").trim()
+            para++
+
+            lessons.push({time, subject, type, room, teacher, text, links})
+        })
+
+    } catch (err) {
+        console.error(err)
+    }
+    await browser.close()
+
+    if (lessons.length >= 2) {
+        if (equalObject(lessons[lessons.length - 1], lessons[lessons.length - 2]))
+            return lessons.slice(0, lessons.length - 1)
+        return lessons
+    }
     return lessons
 }
